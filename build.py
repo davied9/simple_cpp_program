97284f9e1224194ecbee587167b2a03b2cafec14
davied <wuwei_543333827@126.com>
2019-09-10 00:43:43 +0800

    implementation of docker build procedure improve argument parser with parser group

--- build.py
+++ build.py
@@ -28,15 +28,18 @@ class CMakeCPPBuilder(object):
         try:
             self.parse_args(args = args)
             self.setup_logger()
-            self.check_build_environment()
-            if 'Windows' == platform.system():
-                self.configure_build_win()
-                self.start_build_win()
-                self.resotre_env()
-            elif 'Linux' == platform.system():
-                self.configure_build_lin()
-                self.start_build_lin()
-                self.resotre_env()
+            if self.build_with_docker:
+                self.start_build_with_docker()
+            else:
+                self.check_build_environment()
+                if 'Windows' == platform.system():
+                    self.configure_build_win()
+                    self.start_build_win()
+                    self.resotre_env()
+                elif 'Linux' == platform.system():
+                    self.configure_build_lin()
+                    self.start_build_lin()
+                    self.resotre_env()
         except Exception:
             exc_type, exc_value, exc_traceback = sys.exc_info() # extract most recent Exception info fro sys
             self.logger.info('##############################################################################################')
@@ -66,45 +69,55 @@ class CMakeCPPBuilder(object):
         parser = ArgumentParser(
             prog=__file__,
             formatter_class=RawTextHelpFormatter,
-            description='build CXX program with cmake + ninja/nmake/msbuild/make in win32/linux',
+            description='build CXX program with cmake + msbuild/make/ninja in win32/linux',
             epilog='''
 [example]
     {0}                     # build with default setttings
     {0} install --verbose   # build install target with verbose log
 '''.format(__file__))
 
-        # version info
+        # arguments
+        ### common
         parser.add_argument('-v', '--version', help='version', action='version', version='0.1.0')
-        
-        # common
         parser.add_argument('-vv', '--verbose', help='increase log verbose level', default=False, action='store_true')
-        parser.add_argument('-d', '--Debug', help='build Debug target', default=False, action='store_true')
-        parser.add_argument('-R', '--RelWithDebInfo', help='build RelWithDebInfo target', default=False, action='store_true')
-        parser.add_argument('-r', '--RelMinSize', help='build RelMinSize target', default=False, action='store_true')
+        parser.add_argument('-n', '--no-log-file', help='do not generate log file', default=False, action='store_true')
         parser.add_argument('-c', '--clean', help='''clean build directory after build is done,
 if you need to clean target, pass clean as a target''', default=False, action='store_true')
         parser.add_argument('-C', '--rebuild', help='clean build directory before start build', default=False, action='store_true')
-        parser.add_argument('-D', '--build-with-docker', help='''build with docker toolchain container''', default=False, action='store_true')
-        parser.add_argument('-I', '--docker-toolchain-image', help='''docker image of build toolchain,
-default is davied9/dpc_build_toolchain_centos:latest''', default='davied9/dpc_build_toolchain_centos:latest')
-        parser.add_argument('-t', '--build-tool', help='''build toot, the target cmake generate for,
+        ### build
+        build = parser.add_argument_group('build configurations')
+        build.add_argument('-d', '--Debug', help='build Debug target', default=False, action='store_true')
+        build.add_argument('-R', '--RelWithDebInfo', help='build RelWithDebInfo target', default=False, action='store_true')
+        build.add_argument('-r', '--RelMinSize', help='build RelMinSize target', default=False, action='store_true')
+        build.add_argument('-T', '--build-type', help='build type : Release[default] Debug RelWithDebInfo RelMinSize', default='Release')
+        build.add_argument('-t', '--build-tool', help='''build toot, the target cmake generate for,
 default Windows build tool is Visual Studio,
-default Linux build tool is GNU make''', default=self.build_tool)
-        parser.add_argument('-s', '--source-directory', help='source directory, where CMakeLists.txt lies', default=os.getcwd())
-        parser.add_argument('-w', '--build-directory', help='''build directory, which will be create and used 
-as work directory alongside CMakeLists.txt lies''', default=self.build_dir)
-        parser.add_argument('targets', help='tagets to build', default=[], nargs='*')
-        # linux
-        # windows
-        parser.add_argument('-M', '--msvc-version', help='''Microsoft Visual Studio Version
+default Linux build tool is GNU make''', default=None)
+        build.add_argument('-s', '--source-directory', help='source directory, where CMakeLists.txt lies', default=os.getcwd())
+        build.add_argument('-w', '--build-directory', help='''build directory, which will be create and used 
+as work directory alongside CMakeLists.txt lies''', default=None)
+        build.add_argument('targets', help='targets to build', default=[], nargs='*')
+        ### docker
+        docker = parser.add_argument_group('docker configurations')
+        docker.add_argument('-D', '--build-with-docker', help='''build with docker toolchain container''', default=False, action='store_true')
+        docker.add_argument('-I', '--docker-toolchain-image', help='''docker image of build toolchain,
+default is davied9/dpc_build_toolchain_centos:latest''', default='davied9/dpc_build_toolchain_centos:latest')
+        docker.add_argument('-W', '--docker-mapped-path', help='''where source directory is to be mounted inside the docker image,
+default is /build_area''', default='/build_area')
+        ### linux
+        linux = parser.add_argument_group('linux configurations')
+        ### windows
+        windows = parser.add_argument_group('windows configurations')
+        windows.add_argument('-M', '--msvc-version', help='''Microsoft Visual Studio Version
 Visual Studio 2019 [specify 2019 or 16 as MSVC_VERSION]
 Visual Studio 2017 [specify 2017 or 15 as MSVC_VERSION]
 Visual Studio 2015 [specify 2015 or 14 as MSVC_VERSION]
 Visual Studio 2013 [specify 2013 or 12 as MSVC_VERSION]
 Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
-''', default=2012)
+''', default='2019')
         
         # parse
+        # self.logger.error('parsing from {0}'.format(args))
         arguments = parser.parse_args(args=args)
         
         # validate
@@ -112,29 +125,85 @@ Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
         n_build_type_flags += 1 if arguments.Debug else 0
         n_build_type_flags += 1 if arguments.RelWithDebInfo else 0
         n_build_type_flags += 1 if arguments.RelMinSize else 0
+        n_build_type_flags += 1 if arguments.build_type != 'Release' else 0
         if n_build_type_flags > 1:
             raise Exception('too many build flags are set')
+        ### replace path separator with '/'
+        arguments.source_directory = arguments.source_directory.replace(os.path.sep, '/')
+        arguments.build_directory = arguments.build_directory.replace(os.path.sep, '/') if arguments.build_directory else None
+        arguments.docker_mapped_path = arguments.docker_mapped_path.replace(os.path.sep, '/')
         
         # apply configurations
-        self.targets = arguments.targets
+        ### common
         self.logger.level = logging.DEBUG if arguments.verbose else logging.INFO
-        self.build_type = 'Release' # Release Debug RelWithDebInfo RelMinSize
+        self.no_log_file = arguments.no_log_file
+        self.clean_after_build = arguments.clean # cleanup build directory after done build
+        self.clean_before_build = arguments.rebuild # cleanup build directory before start build
+        ### build
+        self.build_type = arguments.build_type # Release Debug RelWithDebInfo RelMinSize
         if arguments.Debug:
             self.build_type = 'Debug'
         elif arguments.RelWithDebInfo:
             self.build_type = 'RelWithDebInfo'
         elif arguments.RelMinSize:
             self.build_type = 'RelMinSize'
-        self.clean_before_build = arguments.rebuild # cleanup build directory before start build
-        self.clean_after_build = arguments.clean # cleanup build directory after done build
+        self.build_tool = arguments.build_tool if arguments.build_tool else self.build_tool
+        self.source_dir = arguments.source_directory
+        self.build_dir = self.source_dir + '/' + ( arguments.build_directory if arguments.build_directory else self.build_dir )
+        self.target_architecture = 'x64'
+        ### docker
         self.build_with_docker = arguments.build_with_docker
         self.docker_toolchain_image = arguments.docker_toolchain_image # if specified, docker toolchain will be used as build toolchain
-        self.build_tool = arguments.build_tool
-        self.source_dir = arguments.source_directory
-        self.build_dir = os.path.join(self.source_dir, arguments.build_directory)
+        self.docker_mapped_path = arguments.docker_mapped_path
+        ### linux
+        ### windows
         self.msvc_version = int(arguments.msvc_version)
-        self.target_architecture = 'x64'
+        ### target
+        self.targets = arguments.targets
         
+        # make docker argument
+        docker_arguments = ''
+        if self.build_with_docker:
+            ### common
+            docker_arguments += ' --verbose ' if arguments.verbose else ''
+            docker_arguments += ' --no-log-file '
+            docker_arguments += ' --clean ' if arguments.clean else ''
+            docker_arguments += ' --rebuild ' if arguments.rebuild else ''
+            ### build
+            docker_arguments += ' --build-type ' + self.build_type
+            docker_arguments += ' --build-tool "{0}" '.format(arguments.build_tool) if arguments.build_tool else ''
+            #   in docker image, source directory is where source directory is mapped
+            docker_arguments += ' --source-directory "{0}" '.format(arguments.docker_mapped_path)
+            docker_arguments += ' --build-directory "{0}" '.format(arguments.build_directory)  if arguments.build_directory else ''
+            ### docker -- do not copy docker configurations, it's a loop
+            ### linux
+            ### windows
+            docker_arguments += ' --msvc-version ' + arguments.msvc_version
+            for t in arguments.targets:
+                docker_arguments += ' ' + t
+        self.docker_arguments = docker_arguments
+        
+    ##############################################################################################
+    # docker build procedure
+    ##############################################################################################
+    def start_build_with_docker(self):
+        '''
+        entry for build with docker
+        '''
+        self.log_build_configuration()
+        docker_command = ['docker', 'run', '--rm', '-t', \
+            '--mount', 'type=bind,source={0},target={1}'.format(self.source_dir, self.docker_mapped_path), \
+            self.docker_toolchain_image, \
+            'bash', '-c', 'cd "{0}" && python -m build {1}'.format(self.docker_mapped_path, self.docker_arguments) \
+        ]
+        self.logger.info('**********************************************************************************************')
+        self.logger.info('* start building with docker image {0} at {1}'.format(self.docker_toolchain_image, self.get_time_stamp()))
+        self.logger.info('**********************************************************************************************')
+        self.run_shell_command(docker_command)
+        self.logger.info('**********************************************************************************************')
+        self.logger.info('* done building with docker at {0}'.format(self.get_time_stamp()))
+        self.logger.info('**********************************************************************************************')
+    
     ##############################################################################################
     # windows build procedure
     ##############################################################################################
@@ -181,7 +250,7 @@ Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
         entry for build program
         '''
         self.log_build_configuration()
-        self.logger.info('start building at {0}'.format(self.get_time_stamp()))
+        self.logger.info('start building windows target at {0}'.format(self.get_time_stamp()))
         if self.clean_before_build:
             if os.path.exists(self.build_dir):
                 self.logger.info('removing ' + self.build_dir)
@@ -190,7 +259,7 @@ Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
             os.makedirs(self.build_dir)
         os.chdir(self.build_dir)
         if os.path.exists('CMakeCache.txt'):
-            self.logger.info('removing ' + os.path.join(self.build_dir, 'CMakeCache.txt'))
+            self.logger.info('removing ' + self.build_dir + '/CMakeCache.txt')
             os.remove('CMakeCache.txt')
         # run cmake
         self.logger.info('##############################################################################################')
@@ -205,14 +274,14 @@ Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
             self.logger.error('[ERROR] c compiler not found')
             raise Exception('c compiler not found')
         self.c_compiler = c_compiler_matchs[0]
-        self.logger.info('C compiler : {0}'.format(self.c_compiler))
+        self.logger.info(' * C compiler : {0}'.format(self.c_compiler))
         # find cxx compiler from cmake log
         cxx_compiler_matchs = re.findall(r'Check for working CXX compiler: ([:/.()\w\s]+)\n', stdout)
         if len(c_compiler_matchs) == 0:
             self.logger.error('[ERROR] cxx compiler not found')
             raise Exception('cxx compiler not found')
         self.cxx_compiler = cxx_compiler_matchs[0]
-        self.logger.info('CXX compiler : {0}'.format(self.cxx_compiler))
+        self.logger.info(' * CXX compiler : {0}'.format(self.cxx_compiler))
         # validate compilers
         c_compiler_dir = os.path.dirname(self.c_compiler)
         cxx_compiler_dir = os.path.dirname(self.cxx_compiler)
@@ -226,8 +295,8 @@ Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
         if 'Tools' != base:
             self.logger.error('[ERROR] Tool dir in cxx_compiler_dir {0} not found, maybe Visual Studio installation path tree changed ?'.format(cxx_compiler_dir))
             raise Exception('Tool dir in cxx_compiler_dir not found')
-        self.auxilary_tool_dir = os.path.join(dir, 'Auxiliary', 'Build')
-        self.logger.info('Auxilary tool path : {0}'.format(self.auxilary_tool_dir))
+        self.auxilary_tool_dir = dir + '/Auxiliary/Build'
+        self.logger.info(' * Auxilary tool path : {0}'.format(self.auxilary_tool_dir))
         # guess solution name
         solution_name = None
         # 1 walk through build dir, find only .sln file
@@ -239,7 +308,7 @@ Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
             break
         if len(sln_files) == 1:
             solution_name = sln_files[0]
-            self.logger.info('Solution located {0}'.format(solution_name))
+            self.logger.info(' * Solution located : {0}'.format(solution_name))
         elif len(sln_files) > 1:
             self.logger.warning('multiple *.sln files located, all not used')
             for sln_file in sln_files:
@@ -250,7 +319,7 @@ Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
             solution_name_matchs = re.findall(r'PROJECT_NAME = (\w+)\n', stdout, flags = re.IGNORECASE)
             if len(solution_name_matchs) == 1:
                 solution_name = solution_name_matchs[0] + '.sln'
-                self.logger.info('Solution located {0}'.format(solution_name))
+                self.logger.info(' * Solution located : {0}'.format(solution_name))
         if not solution_name:
             self.logger.error('[ERROR] solution name not found');
             raise Exception('solution name not found')
@@ -311,7 +380,7 @@ Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
         entry for build program
         '''
         self.log_build_configuration()
-        self.logger.info('start building at {0}'.format(self.get_time_stamp()))
+        self.logger.info('start building linux target at {0}'.format(self.get_time_stamp()))
         if self.clean_before_build:
             if os.path.exists(self.build_dir):
                 shutil.rmtree(self.build_dir)
@@ -363,7 +432,7 @@ Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
             if attr in something_to_hide:
                 to_show = False
             if to_show:
-                self.logger.debug('  {0} : {1}'.format(attr, val))
+                self.logger.debug(' * {0} : {1}'.format(attr, val))
                 
     def resotre_env(self):
         os.chdir(self.source_dir)
@@ -385,6 +454,8 @@ Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
         if platform.python_version().startswith('3'):
             stdout = stdout.decode()
             stderr = stderr.decode()
+        stdout = stdout.replace('\r\n', '\n') # remove duplicated lines
+        stderr = stderr.replace('\r\n', '\n') # remove duplicated lines
         if log_info:
             self.logger.info(stdout)
             self.logger.error(stderr)
@@ -398,8 +469,10 @@ Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
             del fh
             
     def setup_logger(self):
+        if self.no_log_file:
+            return
         self.close_log_files()
-        log_file_path = os.path.join(self.source_dir, 'build_{0}.log'.format(self.get_time_stamp_word()))
+        log_file_path = '{0}/build_{1}.log'.format(self.source_dir, self.get_time_stamp_word())
         self.logger.addHandler(logging.FileHandler(log_file_path))
         
     def init_logger(self):
@@ -435,18 +508,18 @@ Visual Studio 2012 [specify 2012 or 11 as MSVC_VERSION]
             if len(res) == 1:
                 version = res[0]
         if not version:
-            self.logger.debug('{0} : not found'.format(tool_name))
+            self.logger.debug(' * {0:8} : not found'.format(tool_name))
             #self.logger.debug(data_source)
             #self.logger.debug(res)
         else:
-            self.logger.debug('{0:8} : {1}'.format(tool_name, res[0]))
+            self.logger.debug(' * {0:8} : {1}'.format(tool_name, res[0]))
     
     def check_build_environment(self):
         self.logger.debug('##############################################################################################')
         self.logger.debug('# checking build environment')
         self.logger.debug('##############################################################################################')
-        self.logger.debug(platform.platform())
-        self.logger.debug('Python : {0}'.format(platform.python_version()))
+        self.logger.debug(' * ' + platform.platform())
+        self.logger.debug(' * {0:8} : {1}'.format('Python', platform.python_version()))
         self.check_tool('CMake', 'cmake')
         self.check_tool('GNU Make', 'make')
         self.check_tool('GCC', 'gcc')
